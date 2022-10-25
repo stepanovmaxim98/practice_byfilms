4b10b38236896cda97c018ae432312db96101360
Maxim <stepanovmaxim98@yandex.ru>
2022-10-25 19:14:01 +0300

    first changing

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {
+
+    },
+    actors: {
+
+    },
+    genres: [],
+    privat: false
+};
+const lastMovie = prompt('Один из последних просмотренных фильмов?');
+const ballOfMovie = prompt('На сколько оцените его?');
