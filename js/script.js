e78825527eb49ae96afe476e8165124cdc905456
Maxim <stepanovmaxim98@yandex.ru>
2022-11-01 18:38:44 +0300

    With cicles

--- js/script.js
+++ js/script.js
@@ -16,7 +16,27 @@ for(let i = 0; i < 2; i++){
     const b = prompt('Один из последних просмотренных фильмов?');
           a = prompt('На сколько оцените его?');
 
-          personalMovieDB.movies[a] = b;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50 ) {
+        personalMovieDB.movies[a] = b;
+        console.log('Done');
+    } else{
+        console.log('erorr');
+        i--;
+    }
+
+        
+}
+
+if (personalMovieDB.count < 10){
+    console.log("посмотрено мало фильмов");
+
+} else if( personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log('Вы классический зритель');
+
+} else if(personalMovieDB.count >= 30){
+    console.log('Вы киноман!')
+}else {
+    console.log('Ошибка')
 }
 
 console.log(personalMovieDB);
